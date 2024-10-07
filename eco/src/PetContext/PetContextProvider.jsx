77bd926693cc8ee5c0d2f0293b7e1f3649b30714
Navin-VijayaKumar <navinv10122004@gmail.com>
2024-10-07 22:43:08 +0530

    deploy_website

--- eco/src/PetContext/PetContextProvider.jsx
+++ eco/src/PetContext/PetContextProvider.jsx
@@ -3,16 +3,27 @@ import React, { createContext, useState, useEffect } from 'react';
 export const PetContext = createContext(null);
 
 const PetContextProvider = (props) => {
- 
   const [all_product, setall_product] = useState([]);
 
   useEffect(() => {
-    fetch("https://ecoadapation-backend.onrender.com/allproducts")
-      .then((response) => response.json())
+    // Ensure that you're pointing to the backend with HTTPS
+    fetch("https://ecoadapation-backend.onrender.com/allproducts", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then((data) => setall_product(data))
       .catch((error) => console.error('Error fetching products:', error));
   }, []);
-  const contextValue = {all_product};
+
+  const contextValue = { all_product };
 
   return (
     <PetContext.Provider value={contextValue}>
